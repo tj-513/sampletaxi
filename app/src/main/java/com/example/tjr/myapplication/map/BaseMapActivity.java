80b19b4c635829f6e4211d3811ed08220a650427
TJRanaweera <r.tharindu@directfn.com>
2018-07-18 00:56:59 +0530

    first commit

---
+++ app/src/main/java/com/example/tjr/myapplication/map/BaseMapActivity.java
@@ -0,0 +1,12 @@
+package com.example.tjr.myapplication.map;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
